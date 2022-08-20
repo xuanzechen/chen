602bc237eefa294a5b6d9543b95be3fbbd95e24f
xuan <1091603702@qq.com>
2022-08-20 14:15:36 +0800

    xiugai

--- 博客/js/dsad.js
+++
@@ -1,7 +0,0 @@
-import { Empty } from 'antd';
-
-const App = () => <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />;
-
-export default App;
-const empty = createRoot(document.getElementById("empty"));
-empty.render(<App />);
